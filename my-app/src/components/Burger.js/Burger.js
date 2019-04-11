d1024614b73dff2ce4c13ec0430b380ce6385526
helping083 <helping083@gmail.com>
2019-04-11 22:20:27 +0300

    outputting ingredients dynamicly

--- my-app/src/components/Burger.js/Burger.js
+++ my-app/src/components/Burger.js/Burger.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import BurgerBuilder from '../BurgerBuilder/BurgerBuilder';
 
 const burger = (props) => {
+    const transIngredients = Object.keys(props.ingredients)
+      .map((item) => {
+        return [...Array(props.ingredients[item])]
+          .map((i,index) => {
+            return <BurgerIngredient key={item+index} type={item}/>
+          });
+      });
+      console.log('trans', transIngredients);
     return (
         <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top"/>
+        <BurgerIngredient type={'bread-top'}/>
+            {transIngredients}
+        <BurgerIngredient type={'bread-bottom'}/>
         </div>
     );
 };
--- my-app/src/components/BurgerBuilder/BurgerBuilder.js
+++ my-app/src/components/BurgerBuilder/BurgerBuilder.js
@@ -2,16 +2,20 @@ import React, {Component} from 'react'
 import Auxialuary from '../../hoc/auxuilary/Auxialury'
 import Burger from  '../Burger.js/Burger';
 class BurgerBuilder extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}; 
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 2,
+            cheese: 2,
+            meat: 2
+        }
     }
     render() {
         return (
             <Auxialuary>
                 <div>first</div>
                 <div>second</div>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
             </Auxialuary>
         );
     }
