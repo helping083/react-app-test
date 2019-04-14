0cfe43703d8000b41860034b0f4f5e9bcd4db073
helping083 <helping083@gmail.com>
2019-04-14 12:07:30 +0300

    navigation

---
+++ my-app/src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import burgerLogo from '../../assets/img/burger-logo.png';
+import classes from './Logo.css'
+const logo = (props) => (
+  <div className={classes.Logo}>
+      <img src={burgerLogo} alt="logo"/>
+  </div>
+);
+
+export default logo
